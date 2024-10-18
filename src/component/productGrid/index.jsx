cc769aa03eaded4565d954c384a7878f52106573
Hữu Hào <phanhao@192.168.0.186.non-exists.ptr.local>
2024-10-18 11:15:01 +0700

    cart product

--- src/component/productGrid/index.jsx
+++ src/component/productGrid/index.jsx
@@ -10,26 +10,29 @@ const ProductsGridComponent = ({ products }) => {
     <div className="product-list">
       {products.map((product, key) => (
         <div className="product-item" key={product.laptop_ID}>
-          <Link to={ROUTERS.USER.DETAILS} state={{ product }}>
-            <img
-              className="add-to-img"
-              src={product.Image}
-              alt={product.Type_name}
-            />
-          </Link>
-          <button className="add-to-cart">
+          <div className="product-item-image">
+            <Link to={ROUTERS.USER.DETAILS} state={{ product }}>
+              <img
+                className="add-to-img"
+                src={product.Image}
+                alt={product.Type_name}
+              />
+            </Link>
+          </div>
+          <div className="product-item-cart">
             <Link to={ROUTERS.USER.CART} state={{ product }}>
-              Thêm giỏ hàng
+              <h3>Thêm giỏ hàng</h3>
               <AiOutlineShoppingCart />
             </Link>
-          </button>
-
-          <Link to={ROUTERS.USER.DETAILS} state={{ product }}>
-            <div className="item-product-bottom">
-              <h3>{product.Company + " " + product.Type_name}</h3>
-              <p>{product.Price.toLocaleString()}đ</p>
-            </div>
-          </Link>
+          </div>
+          <div className="product-item-bottom">
+            <Link to={ROUTERS.USER.DETAILS} state={{ product }}>
+              <div className="item-product-bottom">
+                <h3>{product.Company + " " + product.Type_name}</h3>
+                <p>{product.Price.toLocaleString()}đ</p>
+              </div>
+            </Link>
+          </div>
         </div>
       ))}
     </div>
--- src/pages/users/cartPage/index.jsx
+++ src/pages/users/cartPage/index.jsx
@@ -4,7 +4,36 @@ import { useLocation } from "react-router-dom";
 const CartPage = () => {
   const location = useLocation();
   const { product } = location.state || {};
-  return <div></div>;
+  return (
+    <div className="cart-page">
+      <div className="container">
+        <div className="row">
+          <div className=" col-lg-3"></div>
+          <div className=" col-lg-6">
+            <div className="cart">
+              <div className="cart-header">
+                <h2>Giỏ hàng của bạn</h2>
+                <div className="cart-content">
+                  <div className="cart-item">
+                    <div className="cart-item-image">
+                      <img src={product.Image} alt={product.Type_name} />
+                    </div>
+                    <div className="cart-item-name">
+                      <h3>{product.Type_name}</h3>
+                    </div>
+                    <div className="cart-item-price">
+                      <h3>{product.Price}</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className=" col-lg-3"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartPage;
